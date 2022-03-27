f17e7b78a0c428c1809fd7337b69a747e724fefa
Silviu Vijiala <obvyk6>
2022-03-27 13:26:40 +0300

    Add seed.js to populate db

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('./startup/validation')();
 // const p = Promise.reject(new Error("Testing how promise rejection is handled!"))
 // p.then(() => console.log('Done'))
 
-const port = process.env.PORT || 3000;
+const port = process.env.vidly_backend_PORT || 3000;
 const server =app.listen(port, () => winston.info(`Listening to port ${port}...`));
 
 module.exports = server;
